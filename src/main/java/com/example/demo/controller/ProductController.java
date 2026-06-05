25a9a1879d09ceaf697d6c5e990f6d4c1f31157e
Esneider <esneider.marquez@udea.edu.co>
2026-06-05 13:15:17 -0500

    Documentacion y unas mejoras

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.example.demo.model.Product;
 import com.example.demo.repository.ProductRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,12 +23,13 @@ public class ProductController {
     @Autowired
     private ProductRepository repository;
     
-    
+    //metodo post para el insertar a la base de datos
     @PostMapping("/product/insert")
     public String insert(@RequestParam String name, @RequestParam Double proteins, @RequestParam Double carbohydrates
         , @RequestParam Double fats, @RequestParam String description, @RequestParam Double price, @RequestParam int stock
         , @RequestParam Double gramsPer, @RequestParam String unityT, @RequestParam String category,RedirectAttributes ra){
         
+        //se crea un producto y se le cambian todos sus atributos por los parametros
         Product p = new Product();
         p.setName(name);
         p.setProteins(proteins);
@@ -40,16 +42,24 @@ public class ProductController {
         p.setUnityT(unityT);
         p.setCategory(category);
 
-        repository.save(p);
-        ra.addFlashAttribute("success", "true");
+        try{
+            //lo guarda en la base de datos con el repository
+            repository.save(p);
+            ra.addFlashAttribute("createSuccess", "true");
+        }catch (DataAccessException d){
+            ra.addFlashAttribute("createError", "true");
+        }
         
         return "redirect:/";
     }
         
+    //metodo post para desactivar los productos
     @PostMapping("/product/delete")
     public String delete(@RequestParam int deleteId, RedirectAttributes ra) {
+        //busca el producto por la id que se recibe del index y lo assigna a una variable p
         Product p = repository.findById(deleteId).orElse(null);
         if(p!=null){
+            //cambia el estado y guarda en la base de datos otra vez
             p.setActive(false);
             repository.save(p);
             ra.addFlashAttribute("deactiveSuccess","true");
@@ -60,11 +70,13 @@ public class ProductController {
         return "redirect:/";
     }
 
-
+    //metodo post para reactivar los productos
     @PostMapping("/product/active")
     public String active(@RequestParam int activeId, RedirectAttributes ra) {
+        //una vez mas busca el producto por la id recibida y lo asigna a una variable p
         Product p = repository.findById(activeId).orElse(null);
         if(p!=null){
+            //cambia el estado y lo guarda en la base de datos
             p.setActive(true);
             repository.save(p);
             ra.addFlashAttribute("reactiveSuccess","true");
@@ -75,20 +87,26 @@ public class ProductController {
         return "redirect:/";
     }
 
+    //metodo get para poder mostrar los productos en el fronted SIEMPRE, solo se necesita abir la web, osea "/".
     @GetMapping("/")
+    // se usa un parametro model creado por spring boot para regresar informacion al frontend.
     public String show(Model model) {
         model.addAttribute("products", repository.findByActiveTrue());
         return "index";
     }
+    // metodo get para poder encontrar un producto por nombre en la base de datos.
     @GetMapping("/product/find")
     public String find(@RequestParam String findName, Model model) {
+        //asigna los productos a una lista results para enviarla mediante model a el index
         List<Product> results= repository.findByNameContainingIgnoreCaseAndActiveTrue(findName);
         model.addAttribute("results", results);
         model.addAttribute("products", repository.findByActiveTrue());
         return "index";
     }
 
+    //metodo get para mostrar todos los atributos de los productos.
     @GetMapping("/product/details")
+    //manda al frontend la lista de productos y los detalles del producto del cual se selecciono para mostrar mas informacion
     public String getMethodName(@RequestParam int id, Model model) {
         model.addAttribute("productDetails", repository.findById(id).orElse(null));
         model.addAttribute("products", repository.findByActiveTrue());
@@ -96,17 +114,22 @@ public class ProductController {
         return "index";
     }
     
-    
+    //metodo get 
     @GetMapping("/product/updateId")  
     public String showUpdate(@RequestParam (required = false)Integer updateIdP, Model model) {
-        if(updateIdP != null){
-            Product product = repository.findById(updateIdP).orElse(null);
-            model.addAttribute("updateProduct", product);
+        try{
+            if(updateIdP != null){
+                Product product = repository.findById(updateIdP).orElse(null);
+                model.addAttribute("updateProduct", product);
+            }
+            model.addAttribute("products", repository.findByActiveTrue());
+        }catch (DataAccessException e){
+            model.addAttribute("updateIdError", "Error loading product");
         }
-        model.addAttribute("productos", repository.findByActiveTrue());        
         return "index";
     }
     
+    //metodo post para actualizar la informacion de un producto
     @PostMapping("/product/update")
     public String updateProduct(@RequestParam int id, @RequestParam String updName,
             @RequestParam Double updProteins, @RequestParam Double updCarbohydrates,
@@ -115,7 +138,9 @@ public class ProductController {
             @RequestParam Double updGramsPer, @RequestParam String updUnityT,
             @RequestParam String updCategory, RedirectAttributes ra) {
 
+        //Asiga a una variable product el producto encontrado por el repositorio con la id recibida.
         Product product = repository.findById(id).orElse(null);
+        //Si el producto si se encontro se acutaliza, de lo contrario retorna el atributo de error
         if (product != null) {
             product.setName(updName);
             product.setProteins(updProteins);
@@ -128,9 +153,9 @@ public class ProductController {
             product.setUnityT(updUnityT);
             product.setCategory(updCategory);
             repository.save(product);
-            ra.addFlashAttribute("success", "true");
+            ra.addFlashAttribute("updSuccess", "true");
         } else {
-            ra.addFlashAttribute("error", "true");
+            ra.addFlashAttribute("updError", "true");
         }
         return "redirect:/";
     } 
